99bd1bc7d4d8bb47aba16790dab58caa079abfa7
mari951 <marieaz843@gmail.com>
2024-10-11 11:40:39 -0300

    Add files via upload

---
+++ Controller/TelaDeAtualizacaoController.java
@@ -0,0 +1,47 @@
+package Controller;
+import model.*;
+import view.*;
+import java.sql.*;
+import java.util.*;
+
+public class TelaDeAtualizacaoController extends TelaDeAtualizacaoView
+public static void popularIds() {
+    // tenho que invocar o método do model que vai popular os ids
+
+}
+ public static void enviarIdsParaView
+
+
+
+   
+                } 
+            }
+        
+
+
+
+
+
+
+
+public static void notificarUsuario
+
+
+        }catch(Exception e) {
+        TelaDeAtualizacaoController.notificarUsuario(
+   
+   
+   
+   
+            public static String setHtmlFormat(String strTexto) {
+        return "<html><body>" + strTexto + "</body></html>";
+    }
+
+    public static void notificarUsuario(String[] args) {
+        TelaDeAtualizacao appTelaDeAtualizacao = new TelaDeAtualizacao();
+        appTelaDeAtualizacao.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+
+    }
+}
